0e1286edf178ecfecf06712ddde180aecd8b287d
Renan Oliveira <renan04lima@outlook.com>
2021-09-09 12:25:40 -0300

    feat: config socket

--- gdrive-webapi/src/index.js
+++ gdrive-webapi/src/index.js
@@ -1,6 +1,7 @@
 import https from 'https'
 import fs from 'fs'
 import { logger } from './logger.js'
+import {  Server } from 'socket.io'
 
 const PORT = process.env.PORT || 3000
 
@@ -8,12 +9,22 @@ const localHostSSL = {
     key: fs.readFileSync('./certificates/key.pem'),
     cert: fs.readFileSync('./certificates/cert.pem'),
 }
+
 const server = https.createServer(
     localHostSSL,
     (req, res) => {
       res.end('hello world')
     }
 )
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        credentials: false
+    }
+})
+
+io.on("connection", (socket) => logger.info(`someone connected: ${socket.id}`))
 
 const startServer = () => {
     const { address, port } = server.address()
