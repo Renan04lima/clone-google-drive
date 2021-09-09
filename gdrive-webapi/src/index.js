35d240745e1e7b0789ee268b5bdba2ec8675e488
Renan Oliveira <renan04lima@outlook.com>
2021-09-09 12:06:34 -0300

    feat: hello world

---
+++ gdrive-webapi/src/index.js
@@ -0,0 +1,23 @@
+import https from 'https'
+import fs from 'fs'
+import { logger } from './logger.js'
+
+const PORT = process.env.PORT || 3000
+
+const localHostSSL = {
+    key: fs.readFileSync('./certificates/key.pem'),
+    cert: fs.readFileSync('./certificates/cert.pem'),
+}
+const server = https.createServer(
+    localHostSSL,
+    (req, res) => {
+      res.end('hello world')
+    }
+)
+
+const startServer = () => {
+    const { address, port } = server.address()
+    logger.info(`app running at https://${address}:${port}`)
+}
+
+server.listen(PORT, startServer)
