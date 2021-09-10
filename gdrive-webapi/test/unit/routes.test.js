98a823937e9286aed0ed928fc0cd15c17dd46c34
Renan Oliveira <renan04lima@outlook.com>
2021-09-10 08:34:51 -0300

    style: fix escope #handle

--- gdrive-webapi/test/unit/routes.test.js
+++ gdrive-webapi/test/unit/routes.test.js
@@ -42,55 +42,55 @@ describe('#Routes test suite', () => {
         await routes.handler(...params.values())
         expect(params.response.end).toHaveBeenCalledWith('hello world')
     })
-  })
 
-  test('it should set any request with CORS enabled', async () => {
-    const routes = new Routes()
-    const params = {
-        ...defaultParams
-    }
-
-    params.request.method = 'inexistent'
-    await routes.handler(...params.values())
-    expect(params.response.setHeader)
-        .toHaveBeenCalledWith('Access-Control-Allow-Origin', '*')
-  })
-
-  test('given method OPTIONS it should choose options route', async () => {
-    const routes = new Routes()
-    const params = {
-        ...defaultParams
-    }
-
-    params.request.method = 'OPTIONS'
-    await routes.handler(...params.values())
-    expect(params.response.writeHead).toHaveBeenCalledWith(204)
-    expect(params.response.end).toHaveBeenCalled()
-  })
-
-  test('given method POST it should choose post route', async () => {
-    const routes = new Routes()
-    const params = {
-        ...defaultParams
-    }
-
-    params.request.method = 'POST'
-    jest.spyOn(routes, routes.post.name).mockResolvedValue()
-
-    await routes.handler(...params.values())
-    expect(routes.post).toHaveBeenCalled()
-  })
-
-  test('given method GET it should choose get route', async () => {
-    const routes = new Routes()
-    const params = {
-        ...defaultParams
-    }
-    jest.spyOn(routes, routes.get.name).mockResolvedValue()
-
-    params.request.method = 'GET'
-    await routes.handler(...params.values())
-    expect(routes.get).toHaveBeenCalled()
+    test('it should set any request with CORS enabled', async () => {
+      const routes = new Routes()
+      const params = {
+          ...defaultParams
+      }
+  
+      params.request.method = 'inexistent'
+      await routes.handler(...params.values())
+      expect(params.response.setHeader)
+          .toHaveBeenCalledWith('Access-Control-Allow-Origin', '*')
+    })
+  
+    test('given method OPTIONS it should choose options route', async () => {
+      const routes = new Routes()
+      const params = {
+          ...defaultParams
+      }
+  
+      params.request.method = 'OPTIONS'
+      await routes.handler(...params.values())
+      expect(params.response.writeHead).toHaveBeenCalledWith(204)
+      expect(params.response.end).toHaveBeenCalled()
+    })
+  
+    test('given method POST it should choose post route', async () => {
+      const routes = new Routes()
+      const params = {
+          ...defaultParams
+      }
+  
+      params.request.method = 'POST'
+      jest.spyOn(routes, routes.post.name).mockResolvedValue()
+  
+      await routes.handler(...params.values())
+      expect(routes.post).toHaveBeenCalled()
+    })
+  
+    test('given method GET it should choose get route', async () => {
+      const routes = new Routes()
+      const params = {
+          ...defaultParams
+      }
+      jest.spyOn(routes, routes.get.name).mockResolvedValue()
+  
+      params.request.method = 'GET'
+      await routes.handler(...params.values())
+      expect(routes.get).toHaveBeenCalled()
+    })
   })
 
 });
