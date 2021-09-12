cdbd20c3920bf073f489f2d79a3d01fb5053368f
Renan Oliveira <renan04lima@outlook.com>
2021-09-12 17:08:57 -0300

    feat: #handleFileBytes should call emit function and it is a transform stream

--- gdrive-webapi/src/uploadHandler.js
+++ gdrive-webapi/src/uploadHandler.js
@@ -7,14 +7,42 @@ export default class UploadHandler {
         this.io = io
         this.socketId = socketId
         this.downloadsFolder = downloadsFolder
+        this.ON_UPLOAD_EVENT = 'file-upload' // NOTE - vai ser ouvido pelo browse
+        this.messageTimeDelay = messageTimeDelay
     }
 
+    /**
+     * usado para controlar a vazão de dados para não quebrar o client
+     */
+    canExecute(lastExecution) {
+        return (Date.now() - lastExecution) >= this.messageTimeDelay
+    }
 
     /**
      * usado pelo onFile para fazer o funil dos bytes
      */
     handleFileBytes(filename) {
-        
+        this.lastMessageSent = Date.now()
+
+        async function* handleData(source) {
+            let processedAlready = 0
+
+            for await (const chunk of source) {
+                yield chunk
+
+                processedAlready += chunk.length
+                if (!this.canExecute(this.lastMessageSent)) {
+                    continue;
+                }
+
+                this.lastMessageSent = Date.now()
+
+                this.io.to(this.socketId).emit(this.ON_UPLOAD_EVENT, { processedAlready, filename })
+                logger.info(`File [${filename}] got ${processedAlready} bytes to ${this.socketId}`)
+            }
+        }
+
+        return handleData.bind(this)
     }
 
     async onFile(fieldname, file, filename) {
