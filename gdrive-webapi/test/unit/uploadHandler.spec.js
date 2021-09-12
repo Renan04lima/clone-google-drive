2e50ff5b75b09015ae328e4a24252cc109102a8a
Renan Oliveira <renan04lima@outlook.com>
2021-09-12 18:25:28 -0300

    test: #handleFileBytes given message timerDelay as 2secs it should emit only two messages during 2 seconds period

--- gdrive-webapi/test/unit/uploadHandler.spec.js
+++ gdrive-webapi/test/unit/uploadHandler.spec.js
@@ -115,6 +115,61 @@ describe('#UploadHandler test suite', () => {
       expect(onWrite).toBeCalledTimes(messages.length)
       expect(onWrite.mock.calls.join()).toEqual(messages.join())
     })
+
+    test('given message timerDelay as 2secs it should emit only two messages during 2 seconds period', async () => {
+      jest.spyOn(ioObj, ioObj.emit.name)
+
+
+      const day = '2021-07-01 01:01'
+      const twoSecondsPeriod = 2000
+
+      // Date.now do this.lastMessageSent em handleBytes
+      const onFirstLastMessageSent = TestUtil.getTimeFromDate(`${day}:00`)
+      // -> hello chegou
+      const onFirstCanExecute = TestUtil.getTimeFromDate(`${day}:02`)
+      const onSecondUpdateLastMessageSent = onFirstCanExecute
+
+      // -> segundo hello, está fora da janela de tempo!
+      const onSecondCanExecute = TestUtil.getTimeFromDate(`${day}:03`)
+
+      // -> world
+      const onThirdCanExecute = TestUtil.getTimeFromDate(`${day}:04`)
+
+
+      TestUtil.mockDateNow(
+        [
+          onFirstLastMessageSent,
+          onFirstCanExecute,
+          onSecondUpdateLastMessageSent,
+          onSecondCanExecute,
+          onThirdCanExecute,
+        ]
+      )
+
+      const messages = ["hello", "hello", "world"]
+      const filename = 'filename.avi'
+      const expectedMessageSent = 2
+
+      const source = TestUtil.generateReadableStream(messages)
+      const handler = new UploadHandler({
+        messageTimeDelay: twoSecondsPeriod,
+        io: ioObj,
+        socketId: '01',
+      })
+
+      await pipeline(
+        source,
+        handler.handleFileBytes(filename)
+      )
+
+
+      expect(ioObj.emit).toHaveBeenCalledTimes(expectedMessageSent)
+
+      const [firstCallResult, secondCallResult] = ioObj.emit.mock.calls
+
+      expect(firstCallResult).toEqual([handler.ON_UPLOAD_EVENT, { processedAlready: "hello".length, filename }])
+      expect(secondCallResult).toEqual([handler.ON_UPLOAD_EVENT, { processedAlready: messages.join("").length, filename }])
+    })
   })
 
   describe('#canExecute', () => {
