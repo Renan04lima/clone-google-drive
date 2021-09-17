cb8706460a5a0c40074044b9e446600d1e0dd48f
Renan Oliveira <renan04lima@outlook.com>
2021-09-16 23:07:49 -0300

    feat[webapp]: connect socket io

--- gdrive-webapp/public/src/connectionManager.js
+++ gdrive-webapp/public/src/connectionManager.js
@@ -3,11 +3,25 @@
  */
 export default class ConnectionManager {
   constructor({ apiUrl }) {
-      this.apiUrl = apiUrl
+    this.apiUrl = apiUrl
+
+    this.ioClient = io.connect(apiUrl, { withCredentials: false })
+    this.socketId = ''
+  }
+
+  configureEvents({ onProgress }) {
+    this.ioClient.on('connect', this.onConnect.bind(this))
+    this.ioClient.on('file-upload', onProgress)
+  }
+
+  onConnect(msg) {
+    console.log('connected!', this.ioClient.id)
+    this.socketId = this.ioClient.id
+
   }
 
   async currentFiles() {
-      const files = (await (await fetch(this.apiUrl)).json())
-      return files
+    const files = (await (await fetch(this.apiUrl)).json())
+    return files
   }
 }
